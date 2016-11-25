5558017f5f0294ecb7351f9fab3174fc2f2d2234
Carl Potter <carl.potter@gmail.com>
2016-11-25 09:08:13 +0000

    add validation & relationship to swim & pool models

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,2 +1,5 @@
 class Pool < ActiveRecord::Base
+  validates :name, :location, :size, presence: true
+  validates :name, uniqueness: true
+  has_many :swim, :inverse_of => :pool
 end
--- app/models/swim.rb
+++ app/models/swim.rb
@@ -1,2 +1,5 @@
 class Swim < ActiveRecord::Base
+  validates :start, :duration, :distance, presence: true
+  validates :start, uniqueness: true
+  belongs_to :pool, :inverse_of => :swim
 end
