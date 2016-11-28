e0da60872011a0df64c03ec467d69f80460a46f9
Carl Potter <carl.potter@gmail.com>
2016-11-28 23:04:53 +0000

    add line charts to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
 
   # GET /
   def index
+    @swims = Swim.all
+    @swims25 = Swim.joins(:pool).where("length = 25")
+    @swims50 = Swim.joins(:pool).where("length = 50")
   end
 end
